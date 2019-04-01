9e15a60ef1bf36316dbf4a05b36e10c1efd5140b
Lela Smith <smith.lelaa@gmail.com>
2019-03-31 21:16:05 -0700

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,84 @@
+function Order(size, style, beverages, meats, vegetables) {
+  this.size = size;
+  this.style = style;
+  this.beverages = beverages;
+  this.meats = meats;
+  this.vegetables = vegetables;
+  this.price;
+}
 
+Order.prototype.calculatePrice = function() {
+  this.price = 10;
+  if (this.size === 'small') {
+    this.price -= 3;
+  } else if (this.size === 'large') {
+    this.price += 3;
+  } else if (this.size === 'x-large') {
+    this.price += 5;
+  }
+
+  if (this.crust === "deep-dish") {
+    this.price += 3;
+  }
+
+  for (var i = 0; i < this.beverages.length; i++) {
+    this.price += 3;
+  }
+
+  for (var i = 0; i < this.meats.length; i++) {
+    this.price += 2.5;
+  }
+  
+  for (var i = 0; i < this.vegetables.length; i++) {
+    this.price += 1.5;
+  }
+}
+
+Order.prototype.shortDescription = function() {
+  return "A " + this.size + " " + this.style + " pizza: $" + this.price;
+}
+
+var displayOrderDetails = function() {
+  $('#order-details').show();
+  $('#order-details h4').text(newOrder.shortDescription());
+  $('#meats').text(newOrder.meats.join(", "));
+  $('#vegetables').text(newOrder.vegetables.join(", "));
+  $('#beverages').text(newOrder.beverages.join(", "));
+}
+
+$(document).ready(function() {
+  $('#order-form').submit(function(event) {
+    event.preventDefault();
+
+    var size = $('input[name=size]:checked').val();
+    var style = $('input[name=style]:checked').val();
+    var beverages = $('input:checkbox[name=beverages]:checked').map(function() {
+        return this.value;
+      }).get();
+    var meats = $('input:checkbox[name=meats]:checked').map(function() {
+        return this.value;
+      }).get();
+    var vegetables = $('input:checkbox[name=vegetables]:checked').map(function() {
+        return this.value;
+      }).get();
+    var newOrder = new Order(size, style, beverages, meats, vegetables);
+
+    newOrder.calculatePrice();
+
+    $('#order-list').append('<li><span class="order">' + newOrder.shortDescription() + '</span></li>');
+
+    document.getElementById("order-form").reset();
+
+    console.log();
+
+    $(".order").last().click(function() {
+      $('#order-details').show();
+      $('#order-details h4').text(newOrder.shortDescription());
+      $('#meats').text(newOrder.meats.join(", "));
+      $('#vegetables').text(newOrder.vegetables.join(", "));
+      $('#beverages').text(newOrder.beverages.join(", "));
+    });
+  });
+
+
+});
